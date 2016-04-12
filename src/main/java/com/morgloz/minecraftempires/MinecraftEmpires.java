446113b4acd6fd15aa0ec5531ffacd13f0fc60eb
Morgloz <alejandromvlm@gmail.com>
2016-04-12 02:30:39 +0200

    Day 1 commit

---
+++ src/main/java/com/morgloz/minecraftempires/MinecraftEmpires.java
@@ -0,0 +1,32 @@
+package com.morgloz.minecraftempires;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="MinecraftEmpires", name="Minecraft Empires", version="1.8.9-1.0")
+public class MinecraftEmpires
+{
+
+    @Mod.Instance("MinecraftEmpires")
+    public static MinecraftEmpires instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
